afaf8a0ddedc33149197ca82e433fa792b55a465
alejoarias25 <77022007+alejoarias25@users.noreply.github.com>
2021-02-17 21:31:03 -0500

    A continuacion dejo evidencia de mi 5 funcionalidad de Apliboston, - La funcionalidad es Cerrar Sesion para salir de ApliBoston, tanto para estudiantes y docentes. Alejandro Arias Arias.

--- app/src/main/java/coleg/ejerc/colegioboston/MenuActivity.java
+++ app/src/main/java/coleg/ejerc/colegioboston/MenuActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MenuActivity extends AppCompatActivity {
 
     ImageView img_estudiantes;
+    Button btn_cerrar;
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,19 @@ public class MenuActivity extends AppCompatActivity {
 
             }
         });
+
+        mAuth=FirebaseAuth.getInstance(); btn_cerrar = findViewById(R.id.btn_cerrar);
+
+        btn_cerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+        public void onClick(View v) { mAuth.signOut();
+            Intent i = new Intent(MenuActivity.this,MainActivity.class); startActivity(i);
+            finish();
+
+
+        }
+        });
+
 
 
 
