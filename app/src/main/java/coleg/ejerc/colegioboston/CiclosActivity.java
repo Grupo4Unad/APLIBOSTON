78ec78a4368c845d9a85a1c97b459df6e9caf166
alejoarias25 <77022007+alejoarias25@users.noreply.github.com>
2021-02-23 09:26:25 -0500

    A continuación subo layout para modificar el acceso a la guía de actividades de la app. Alejandro Arias Arias

---
+++ app/src/main/java/coleg/ejerc/colegioboston/CiclosActivity.java
@@ -0,0 +1,14 @@
+package coleg.ejerc.colegioboston;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CiclosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ciclos);
+    }
+}
